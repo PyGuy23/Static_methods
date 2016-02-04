3f3450eaa63904007e4dbd9c4bb524af7627ccd0
Timothy <eclisesolaris23@gmail.com>
2016-02-04 14:43:30 -0500

    Initial Commit

---
+++ Static_methods/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Static_methods
+//
+//  Created by newuser on 2/4/16.
+//  Copyright © 2016 Static_methods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
